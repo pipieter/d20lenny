32171c171ef728128fbf3b773bf3b96373acaa0d
Pim Pieters <pim@pieters.tv>
2026-04-14 14:46:54 +0200

    Update main file

--- d20/__main__.py
+++ d20/__main__.py
@@ -1,5 +1,17 @@
 from d20 import roll
 
 while True:
-    roll_result = roll(input(), allow_comments=True)
-    print(str(roll_result))
+    expr = input("> ").strip().lower()
+
+    if expr == "":
+        continue
+
+    stop_words = ["quit", "stop", "halt", "exit"]
+    if expr in stop_words:
+        break
+
+    try:
+        roll_result = roll(expr, allow_comments=True)
+        print(str(roll_result))
+    except:
+        print(f"Could not parse '{expr}'")
