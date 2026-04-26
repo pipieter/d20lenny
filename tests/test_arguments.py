d9f26fed16a02be2a260b02caee4511b60b73bea
Pim Pieters <pim@pieters.tv>
2026-04-27 01:01:26 +0200

    Rework advantage tests

--- tests/test_arguments.py
+++ tests/test_arguments.py
@@ -2,31 +2,30 @@ from d20 import *
 
 
 def test_advantage():
+    r = roll("1d20", advantage=AdvType.NONE)
+    assert 1 <= r.total <= 20
+    assert len(r.rolls) == 1
+    assert len(r.warnings) == 0
+
     r = roll("1d20", advantage=AdvType.ADV)
     assert 1 <= r.total <= 20
-    assert r.result.startswith("2d20kh1 ")
+    assert len(r.rolls) == 2
+    assert len(r.warnings) == 0
+    assert r.total == max(rr.total for rr in r.rolls)
 
     r = roll("1d20", advantage=AdvType.DIS)
     assert 1 <= r.total <= 20
-    assert r.result.startswith("2d20kl1 ")
-
-    r = roll("1d20", advantage=AdvType.NONE)
-    assert 1 <= r.total <= 20
-    assert r.result.startswith("1d20 ")
+    assert len(r.rolls) == 2
+    assert len(r.warnings) == 0
+    assert r.total == min(rr.total for rr in r.rolls)
 
     # adv/dis should do nothing on non-d20s
     r = roll("1d6", advantage=AdvType.ADV)
     assert 1 <= r.total <= 6
-    assert r.result.startswith("1d6 ")
+    assert len(r.rolls) == 1
+    assert len(r.warnings) == 1
 
     r = roll("1d6", advantage=AdvType.DIS)
     assert 1 <= r.total <= 6
-    assert r.result.startswith("1d6 ")
-
-
-def test_rolling_ast():
-    the_ast = parse("1d20")
-    r = roll(the_ast)
-
-    assert 1 <= r.total <= 20
-    assert r.result.startswith("1d20 ")
+    assert len(r.rolls) == 1
+    assert len(r.warnings) == 1
